7aaa370a9d92174f0c4cc23a8c3d37464c2adeef
sislac1986 <68289251+sislac1986@users.noreply.github.com>
2021-02-18 16:09:23 -0300

    Update Forecast.js

--- JS/Forecast.js
+++ JS/Forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor() {
         this.key = 'oUHBzQZjvQjyuNjWDFg6y0hm5zDCuX7r';
-        this.weatherURI = `https://dataservice.accuweather.com/currentconditions/v1/`;
-        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = `http://dataservice.accuweather.com/currentconditions/v1/`;
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     }
 
     async updateCity(city) {
