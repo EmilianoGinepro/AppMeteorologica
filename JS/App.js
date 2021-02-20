530c5d93088c6d15f362ffbcf94aa8349836fe15
sislac1986 <68289251+sislac1986@users.noreply.github.com>
2021-02-20 16:33:30 -0300

    cambios minimos
    saque algunos comentarios

--- JS/App.js
+++ JS/App.js
@@ -8,10 +8,7 @@ const forecast = new Forecast();
 const updateUI = (data) => {
 
     console.log(data);
-    // const cityDetails = data.cityDetails;
-    // const weather = data.weather;
-
-    // destructuring
+  
     const { cityDetails, weather } = data;
 
     details.innerHTML = `
@@ -23,32 +20,26 @@ const updateUI = (data) => {
         </div>
     `;
 
-    // update day/night img and icon
     const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
     icon.setAttribute('src', iconSrc);
-
-    // ternary operator
+ 
     let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
     timeImg.setAttribute('src', timeSrc);
-
-    // check if d-none is present
+  
     if (card.classList.contains('d-none')) {
         card.classList.remove('d-none');
     }
 };
 
 cityForm.addEventListener('submit', e => {
-    // prevent page refresh
+    
     e.preventDefault()
-
-    // get input value (city)
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    // update UI with new city
+   
     forecast.updateCity(city)
         .then(data => updateUI(data))
-        // .then(data => console.log(data))
         .catch(err => console.log(err));
 
     // store latest city in local storage
